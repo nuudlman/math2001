0c9dec58d5a305b187c71cf6759f8bef19a25474
Isaac Nudelman <isaac.nudelman@utexas.edu>
2024-07-01 12:24:17 -0500

    example 1.3.10

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -77,7 +77,10 @@ example {a b : ℚ} (h1 : a - 3 = 2*b) : a^2 - a + 3 = 4*b^2 + 10*b + 9 :=
 
 -- Example 1.3.10
 example {z : ℝ} (h1 : z^2 - 2 = 0) : z^4 - z^3 - z^2 + 2*z + 1 = 3 :=
-  sorry
+  calc
+    z^4 - z^3 - z^2 + 2*z + 1 = (z^2 - z + 1)*(z^2 - 2) + 3 := by ring
+    _ = (z^2 - z + 1)*0 +3 := by rw [h1]
+    _ = 3 := by ring
 
 /-! # Exercises
 
