127c625aa2c2d14929ca9c798c7cd8f79293ff2e
Isaac Nudelman <isaac.nudelman@utexas.edu>
2024-07-01 12:21:39 -0500

    example 1.3.9

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -70,7 +70,10 @@ example {x y : ℝ} (h1 : x + y = 4) (h2 : 5*x - 3*y = 4) : x = 2 :=
 
 -- Example 1.3.9
 example {a b : ℚ} (h1 : a - 3 = 2*b) : a^2 - a + 3 = 4*b^2 + 10*b + 9 :=
-  sorry
+  calc
+    a^2 - a + 3 = (a - 3)^2 + 5*(a - 3) + 9 := by ring
+    _ = (2*b)^2 + 5*(2*b) + 9 := by rw [h1]
+    _ = 4*b^2 + 10*b + 9 := by ring
 
 -- Example 1.3.10
 example {z : ℝ} (h1 : z^2 - 2 = 0) : z^4 - z^3 - z^2 + 2*z + 1 = 3 :=
